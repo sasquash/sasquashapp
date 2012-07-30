7a1ad946ec040e0125f36a208e0e81073443da21
Pedro Aspiazu <pedro@tumami.com>
2012-07-31 00:39:43 +0200

    as

--- js/main.js
+++ js/main.js
@@ -171,21 +171,7 @@ function toggleCompass() {
 
 
 
-    // Called when capture operation is finished
-    //
-    function captureSuccess(mediaFiles) {
-        var i, len;
-        for (i = 0, len = mediaFiles.length; i < len; i += 1) {
-            uploadFile(mediaFiles[i]);
-        }       
-    }
 
-    // Called if something bad happens.
-    // 
-    function captureError(error) {
-        var msg = 'An error occurred during capture: ' + error.code;
-        navigator.notification.alert(msg, null, 'Uh oh!');
-    }
 
     // A button will call this function
     //
